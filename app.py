e896c14fe61981e031ab6f0b10b17e803f5f36e3
Katarzyna Debowska <katarzyna.debowska@host.com>
2023-05-13 20:12:01 +0200

    add session to prevent cheating (not working)

--- app.py
+++ app.py
@@ -22,9 +22,11 @@ chimpTest = ChimpTest()
 
 @app.route('/chimpTest/start')
 def chimp_test_start():
+    session.clear()  # Wyczyść sesję przy rozpoczęciu testu
     chimpTest.player.chimpTest_score = 0
     chimpTest.numbers = 3
     return render_template('chimpTest/start.html')
+
 
 @app.route('/chimpTest/task')
 def chimp_test_task():
@@ -32,22 +34,30 @@ def chimp_test_task():
     chimpTest.player.chimpTest_answers = []
     chimpTest.numbers += 1
     chimpTest.choose_buttons()
+
+
+    session['last_page'] = 'task'  # Zapisz informację o ostatniej odwiedzonej stronie w sesji
+
     return render_template('chimpTest/task.html', chimpTest=chimpTest)
 
-
-@app.route('/chimpTest/answer/<int:index>')
-def chimp_test_answer(index):
-    chimpTest.player.give_chimp_answers(chimpTest.visible_buttons[index])
+@app.route('/chimpTest/answer/<int:value>')
+def chimp_test_answer(value):
+    print('1session', session['last_page'])
+    print("1chimpTest.player.chimpTest_answers", chimpTest.player.chimpTest_answers)
+    if chimpTest.player.chimpTest_answers and session.get('last_page') != 'answer':
+        print('session', session['last_page'])
+        print("chimpTest.player.chimpTest_answers", chimpTest.player.chimpTest_answers)
+        # return redirect('/chimpTest/end', code=302)
+    chimpTest.player.give_chimp_answers(value)
+    print('session', session['last_page'])
     if len(chimpTest.player.chimpTest_answers) == len(chimpTest.visible_buttons):
         return redirect('/chimpTest/task')
-    print('player answers', chimpTest.player.chimpTest_answers)
-    print('chimpTest.visible_buttons', chimpTest.visible_buttons)
+    session['last_page'] = 'answer'  # Zapisz informację o ostatniej odwiedzonej stronie w sesji
     return redirect('/chimpTest/check_answer', code=302)
-
 
 @app.route('/chimpTest/check_answer')
 def chimp_test_check_answer():
-    i = len(chimpTest.player.chimpTest_answers)-1
+    i = len(chimpTest.player.chimpTest_answers) - 1
     if chimpTest.player.chimpTest_answers[i] == chimpTest.visible_buttons[i]:
         return render_template('chimpTest/task.html', chimpTest=chimpTest)
     else:
@@ -55,6 +65,7 @@ def chimp_test_check_answer():
 
 @app.route('/chimpTest/end')
 def chimp_test_end():
+    session.clear()  # Wyczyść sesję przy zakończeniu testu
     return render_template('chimpTest/end.html', chimpTest=chimpTest)
 
 
