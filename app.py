adbfec5b83a4dfefec222815a7fa22c2b86efbbb
Katarzyna Debowska <katarzyna.debowska@host.com>
2023-05-20 13:46:06 +0200

    add login required and flash messages

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask import request
 from flask import render_template
 from flask import session
 from flask import url_for
+from flask import flash
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
 
 from inc.formula import Formula
 from inc.colorCraze import ColorCraze
@@ -35,6 +37,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://{db_username}:{db_password}@lo
 
 db = SQLAlchemy(app)
 
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String, unique=True, nullable=False)
@@ -46,13 +49,14 @@ class Users(db.Model):
         self.email = email
         self.password = password
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/registration',  methods=['GET', 'POST'])
+
+@app.route('/registration', methods=['GET', 'POST'])
 def register():
-    error = ''
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
@@ -62,23 +66,24 @@ def register():
 
         existing_user = Users.query.filter_by(username=username).first()
         if existing_user:
-            error = "Użytkownik o tej nazwie już istnieje. Wybierz inną nazwę użytkownika."
-            return render_template('registration.html', error=error)
+            flash("Użytkownik o tej nazwie już istnieje. Wybierz inną nazwę użytkownika.")
+            return render_template('registration.html')
 
         if repeat_password != password:
-            error = "Wartości pól hasło i powtórz hasło muszą być takie same"
-            return render_template('registration.html', error=error)
+            flash("Wartości pól hasło i powtórz hasło muszą być takie same.")
+            return render_template('registration.html')
 
         new_user = Users(username=username, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-
+        flash('Rejestracja przebiegła pomyślnie. Zaloguj się na swoje konto.')
         return redirect(url_for('login'))
-    return render_template('registration.html', error=error)
+    return render_template('registration.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = ''
+    session.pop('user_id', None)
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -88,9 +93,32 @@ def login():
             session['user_id'] = user.id
             return redirect(url_for('home'))
         else:
-            error = 'Nazwa użytkownika lub hasło są niepoprawne'
-            return render_template('login.html', error=error)
-    return render_template('login.html', error=error)
+            flash('Nazwa użytkownika lub hasło są niepoprawne.')
+            return render_template('login.html')
+    return render_template('login.html')
+
+
+@app.context_processor
+def inject_variables():
+    user_id = session.get('user_id', 0)
+    return dict(user_id=user_id)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('Zostałeś poprawnie wylogowany.')
+    return redirect(url_for('login'), code=302)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('Musisz się zalogować, aby skorzystać z tej strony.')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 # CHIMP TEST ################################################################################################
@@ -98,7 +126,9 @@ def login():
 # global chimpTest
 chimpTest = ChimpTest()
 
+
 @app.route('/chimpTest/start')
+@login_required
 def chimp_test_start():
     chimpTest.player.chimpTest_score = 0
     chimpTest.numbers = 4
@@ -106,13 +136,16 @@ def chimp_test_start():
 
 
 @app.route('/chimpTest/task')
+@login_required
 def chimp_test_task():
     chimpTest.player.chimpTest_score = len(chimpTest.player.chimpTest_answers)
     chimpTest.player.chimpTest_answers = []
     chimpTest.choose_buttons()
     return render_template('chimpTest/task.html', chimpTest=chimpTest)
 
+
 @app.route('/chimpTest/answer/<int:value>')
+@login_required
 def chimp_test_answer(value):
     referer = request.headers.get('Referer')
     if referer and referer.endswith('/chimpTest/task') and chimpTest.player.chimpTest_answers:
@@ -123,7 +156,9 @@ def chimp_test_answer(value):
         return redirect(url_for('chimp_test_task'))
     return redirect(url_for('chimp_test_check_answer'), code=302)
 
+
 @app.route('/chimpTest/check_answer')
+@login_required
 def chimp_test_check_answer():
     i = len(chimpTest.player.chimpTest_answers) - 1
     if chimpTest.player.chimpTest_answers[i] == chimpTest.visible_buttons[i]:
@@ -131,27 +166,28 @@ def chimp_test_check_answer():
     else:
         return redirect(url_for('chimp_test_end'), code=302)
 
+
 @app.route('/chimpTest/end')
+@login_required
 def chimp_test_end():
     return render_template('chimpTest/end.html', chimpTest=chimpTest)
 
 
 # Color Craze ########################################################################################################
-global colors
+
 colors = ColorCraze()
-# colors.choose_task_type()
-# colors.choose_task()
-# colors.choose_correct_answer()
-# colors.generate_answers()
 
 
 @app.route('/colors/start')
+@login_required
 def colors_start():
     session['time_left'] = 30
     colors.player.colors_points = 0
     return render_template('colorCraze/start.html')
 
+
 @app.route('/colors/question')
+@login_required
 def colors_question():
     time_left = session.get('time_left')
     colors.task_type = ''
@@ -166,40 +202,42 @@ def colors_question():
 
     return render_template('colorCraze/question.html', colors=colors, time_left=time_left)
 
+
 @app.route('/colors/answer/<key>')
+@login_required
 def colors_answer(key):
     time_left = session.get('time_left')
     if key not in colors.answers.keys():
         return render_template('colorCraze/question.html', colors=colors, time_left=time_left)
     answer = [key, colors.answers[key]]
     colors.player.give_colors_answer(answer)
-    print('colors.player.colors_answer', colors.player.colors_answer)
     if len(colors.player.colors_answer) == 2:
-        print('tu powinno przejść do check')
         return redirect(url_for('colors_check_answer'), code=302)
     return redirect(url_for('colors_question'), code=302)
 
 
 @app.route('/colors/check_answer')
+@login_required
 def colors_check_answer():
-    print('colors.player.colors_answer', colors.player.colors_answer)
-    print('colors.correct_answer', colors.correct_answer)
     if sorted(colors.player.colors_answer) == sorted(colors.correct_answer):
         colors.player.colors_points += 1
         return redirect(url_for('colors_question'), code=302)
-    print("bad answer")
-    # session['last_page'] = 'check_answer'
     return render_template('colorCraze/wrong_answer.html', colors=colors)
 
+
 @app.route('/colors/end')
+@login_required
 def colors_end():
     return render_template('colorCraze/end.html', colors=colors)
 
+
 # FORMULA #############################################################################################
-global formula
+
 formula = Formula()
 
+
 @app.route('/formula/start')
+@login_required
 def formula_start():
     session['time_left'] = 30
     formula.player.formula_points = 0
@@ -207,6 +245,7 @@ def formula_start():
 
 
 @app.route('/formula/question')
+@login_required
 def formula_question():
     time_left = session.get('time_left')
     formula.correct_answers = []
@@ -218,31 +257,25 @@ def formula_question():
     formula.calculate_result(correct_answers, operator)
     formula.generate_all_answers()
 
-    session['last_page'] = 'answer'
-
-    print("correct_answers", correct_answers)
-    print("formula.all_answers", formula.all_answers)
-    print("formula.result", formula.result)
-
     return render_template('formula/question.html', formula=formula, time_left=time_left)
 
+
 @app.route('/update_time', methods=['POST'])
+@login_required
 def update_time():
     time_left = request.json['time_left']
     session['time_left'] = time_left
     return "success"
 
+
 @app.route('/formula/answer/<int:index>')
+@login_required
 def formula_answer(index):
     time_left = session.get('time_left')
-    # if session['last_page'] == 'check_answer':
-    #     print('last page', session['last_page'])
-    #     return redirect('/formula/question', code=302)
     if index > 3:
         return render_template('formula/question.html', formula=formula, time_left=time_left)
     if "?" in formula.player.formula_answers:
         formula.player.give_formula_answer(formula.all_answers[index])
-        print('formula.player.formula_answers', formula.player.formula_answers)
         if "?" in formula.player.formula_answers:
             return render_template('formula/question.html', formula=formula, time_left=time_left)
         return redirect(url_for('formula_check_answer'), code=302)
@@ -250,17 +283,16 @@ def formula_answer(index):
 
 
 @app.route('/formula/check_answer')
+@login_required
 def formula_check_answer():
     if sorted(formula.player.formula_answers) == sorted(formula.correct_answers):
-        print('formula.player.formula_answers', formula.player.formula_answers)
-        print('formula.correct_answers', formula.correct_answers)
         formula.player.formula_points += 1
         return redirect(url_for('formula_question'), code=302)
-    print("bad answer")
-    session['last_page'] = 'check_answer'
     return render_template('formula/wrong_answer.html', formula=formula)
 
+
 @app.route('/formula/end')
+@login_required
 def formula_end():
     return render_template('formula/end.html', formula=formula)
 
